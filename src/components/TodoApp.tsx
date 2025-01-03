6b1d24c7b7c008e9d8af94c4bc99ec7798964815
pedro-donoso <pedro.donoso.r@usach.cl>
2025-01-03 12:03:09 -0300

    llamo a componente y modulo

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
 export const TodoApp = () => {
+
+	const [nuevaTarea, setNuevaTarea] = useState('')
+
 	return (
-		<div>TodoApp</div>
+	<div>
+		<h1>Lista de Tareas</h1>
+		<div>
+			<input
+				type="text"
+				value={nuevaTarea}
+				onChange={(e) => setNuevaTarea(e.target.value)}
+				placeholder="Nueva Tarea" />
+		</div>
+	</div>
 	)
-}
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { TodoApp } from "./components/TodoApp";
 
 createRoot(document.getElementById("root")!).render(
 <StrictMode>
-
+	<TodoApp></TodoApp>
 </StrictMode>
 );
